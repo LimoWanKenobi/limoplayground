669efa578db72b96483b22b590740e4da4718c42
Limo Wan Kenobi <limowankenobi@gmail.com>
2015-04-03 00:21:06 +0200

    finished creation of zippy

--- angular2/zippy/src/hello.js
+++ angular2/zippy/src/hello.js
@@ -1,18 +1,14 @@
 import {Component, Template} from 'angular2/angular2';
 import {If} from 'angular2/angular2';
+import {Zippy} from 'zippy';
 
 @Component({
     selector: 'hello'  //TODO: default to camel-cased class name if not provided?
 })
 @Template({
-    inline: `<span *if="name">Hello, {{name}}!</span>`,
-    directives: [If]
+    inline: `<zippy title_attr='My title!'>Content from Hello template!!!</zippy>`,
+    directives: [Zippy]
 })
 export class Hello {
-    name: string = 'World';
-    constructor() {
-        setTimeout(() => {
-          this.name = 'NEW World'
-        }, 2000);
-    }
+    
 }
---
+++ angular2/zippy/src/zippy.js
@@ -0,0 +1,20 @@
+import {Component, Template} from 'angular2/angular2';
+
+@Component({
+  selector: 'zippy',
+  bind: {
+    'title_prop': 'title_attr'
+  }
+})
+@Template({
+  url: 'zippy.html'
+})
+export class Zippy {
+  constructor() {
+    this.visible = true;
+  }
+
+  toggle() {
+    this.visible = !this.visible;
+  }
+}
