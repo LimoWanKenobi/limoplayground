c90f9f97572f32177bf4593dc7288d00378c0752
Limo Wan Kenobi <limowankenobi@gmail.com>
2015-08-17 14:46:55 +0200

    initialized excersim

---
+++ excersim/fsharp/Scripts/load-references.fsx
@@ -0,0 +1,6 @@
+// Warning: generated file; your changes could be lost when a new file is generated.
+#I __SOURCE_DIRECTORY__
+#r @"..\..\packages\NUnit.2.6.3\lib\nunit.framework.dll"
+#r @"System.Core.dll"
+#r @"System.dll"
+#r @"System.Numerics.dll"
