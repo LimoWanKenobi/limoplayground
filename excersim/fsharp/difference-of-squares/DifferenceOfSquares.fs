f856657e8fea37a96433b8231d9f7ccfda0bc68f
Limo Wan Kenobi <limowankenobi@gmail.com>
2015-08-18 09:16:07 +0200

    second exercise. Code compiles

---
+++ excersim/fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,11 @@
+﻿module DifferenceOfSquares
+
+type DifferenceOfSquares(x) =
+    member x.squareOfSums() =
+        0
+
+    member x.sumOfSquares() =
+        0
+
+    member x.difference() =
+        0
