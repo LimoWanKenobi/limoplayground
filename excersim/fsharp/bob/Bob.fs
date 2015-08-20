c4db47b125c33e7c72a848f7479da7e382b91580
Limo Wan Kenobi <limowankenobi@gmail.com>
2015-08-20 15:17:07 +0200

    Finished Bob. First try with active patterns.

--- excersim/fsharp/bob/Bob.fs
+++ excersim/fsharp/bob/Bob.fs
@@ -1,5 +1,25 @@
 ﻿module Bob
 
+open System
+
 type Bob(message) = 
+
+    let hasLetters (str: string) =
+        str
+        |> String.exists Char.IsLetter
+
+    let (|IsYell|_|) (str: string) =
+        if hasLetters str && str.ToUpper() = str then Some(IsYell) else None
+
+    let (|IsQuestion|_|) (str: string) =
+        if str.EndsWith("?") then Some(IsQuestion) else None
+
+    let (|IsSilence|_|) (str: string) =
+        if String.IsNullOrWhiteSpace(str) then Some(IsSilence) else None
+
     member this.hey() =
-        "Whatever."
+        match message with
+        | IsYell -> "Whoa, chill out!"
+        | IsQuestion -> "Sure."
+        | IsSilence -> "Fine. Be that way!"
+        | _ -> "Whatever."
