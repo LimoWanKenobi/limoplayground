9dc7f289861b1463bd7aa82d8b9abd0e47ef0e09
Limo Wan Kenobi <limowankenobi@gmail.com>
2015-08-21 16:30:30 +0200

    cleanup

--- excersim/fsharp/triangle/Triangle.fs
+++ excersim/fsharp/triangle/Triangle.fs
@@ -8,19 +8,24 @@ type TriangleKind =
 type Triangle(a: decimal, b: decimal, c: decimal) =
     let sides = [a; b; c]
 
-    let sum = List.sum sides
+    let sum = a + b + c
+
+    let isValidSide side = sum - side > side
 
     let isValid = 
         sides 
-        |> List.map (fun x -> sum - x > x)
+        |> List.map isValidSide
         |> List.reduce (&&)
 
-    let equalSides = Seq.groupBy id sides
+    let equalSides = 
+        sides
+        |> Seq.distinct
+        |> Seq.length
 
     member this.Kind() = 
         if not isValid then raise (System.InvalidOperationException())
 
-        match Seq.length equalSides with
+        match equalSides with
         | 3 -> TriangleKind.Scalene
         | 2 -> TriangleKind.Isosceles
         | 1 -> TriangleKind.Equilateral
